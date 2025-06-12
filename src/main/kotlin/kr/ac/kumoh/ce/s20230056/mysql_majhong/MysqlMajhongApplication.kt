0734beef0a77c85d0bf62226a6123e185a994aae
sumin <khjksm1223@gmail.com>
2025-06-12 18:41:38 +0900

    💕데이터베이스 연결 성공

---
+++ src/main/kotlin/kr/ac/kumoh/ce/s20230056/mysql_majhong/MysqlMajhongApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.s20230056.mysql_majhong
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MysqlMajhongApplication
+
+fun main(args: Array<String>) {
+	runApplication<MysqlMajhongApplication>(*args)
+}
