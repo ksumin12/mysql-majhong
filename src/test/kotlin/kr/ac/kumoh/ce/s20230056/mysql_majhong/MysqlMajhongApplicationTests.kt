7fd8482a7428240e647e6686d1422fdcde7e10f6
sumin <khjksm1223@gmail.com>
2025-06-12 18:41:38 +0900

    💕데이터베이스 연결 성공

---
+++ src/test/kotlin/kr/ac/kumoh/ce/s20230056/mysql_majhong/MysqlMajhongApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20230056.mysql_majhong
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MysqlMajhongApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
